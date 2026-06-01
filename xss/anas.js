6bf455e5ebc54f90de9029a3687affdd80e01405
Anas Mohamed <ansm8482@gamil.com>
2026-06-01 06:48:18 -0400

    Add xss files to repository

---
+++ xss/anas.js
@@ -0,0 +1,29 @@
+
+                        
+                        
+                        
+//DOM XSS in document.write sink using source location.search
+                        
+                        function trackSearch(query) {
+                            document.write('<img src="/resources/images/tracker.gif?searchTerms='+query+'">'); //not safe
+                            //
+                              //safe code
+
+
+                                 //document.write('<img src="/resources/images/tracker.gif?searchTerms='+encodeURIComponent(query)+'">');
+                            
+                            //
+                        }
+
+
+
+                        var query = (new URLSearchParams(window.location.search)).get('search');
+
+
+                        if(query) {
+                            trackSearch(query);
+                        }
+
+
+                          //slove the lab
+                          //anas" onload="alert(1)
