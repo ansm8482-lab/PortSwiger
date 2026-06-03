9d9b2b96bbf325feefd65b274c5542ce4cee61af
Anas Mohamed <ansm8482@gamil.com>
2026-06-03 02:37:39 -0400

    إضافة ملفات جديدة

---
+++ all/programin/web/JS/script.js
@@ -0,0 +1,45 @@
+// undefinedvalue=undefined
+// console.log(undefinedvalue);
+// document.querySelector('h1').style.color="red" // chang color 
+//  document.write("eng anas")   // whrite words
+//  console.log("fuck isreal")   // print word in console 
+//  window.alert('anas mohamed')   //print word in go to the my page
+// windo have every thng in js and controle every thing in browser
+// window.print(); // print the page
+// window.document.write('hi i eng anas world')
+// document.write(typeof(false)) //spesic type of word
+// var x=10;       //var (variable)
+// console.log (x);
+// let name= "anas";
+// let age=10 ;
+// console.log(name+age);
+// let num=10;
+// num=num+1;
+// console.log(num)
+//convert text to number in JS
+// console.log (typeof(+'4')) 
+// console.log(typeof Number ('4'))
+// console.log(parseFloat ('4.5'))
+
+
+// // big value 
+// console.log(Number.MAX_VALUE) 
+// console.log(Number.MAX_SAFE_INTEGER) 
+//  console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.isSafeInteger(-9007199254740991))
+//chek is number or not
+// console.log(Number.isInteger(-9007199254740991+1)) 
+// console.log(Number.isNaN('anas'/10))
+// convert nubet intger to negative
+// console.log(Math.abs((-5)))  
+// console.log(Math.sqrt((9))) //الجذر التربيعي 
+
+
+// let x=100;
+// console.log(typeof String (x))  //covert number to string 
+// console.log(typeof x.toString ())
+// console.log(typeof (100).toString())
+// console.log(typeof 100.. toString())
+let name='ali';
+console.log(name.repeat(20))  //loopng nume تكرار النص 
+
